0b0175229b3705962b888e0d7279de02ff9d9a67
Lucas Fowler <lucas.fowler@macd.com>
2018-03-14 19:37:06 +0100

    - calculate the velocities for each cell - work out which proportion of the mass moves to the next cells - add these mv entries to the cell list - Then go through and work out all the inelastic collisions

--- main.cpp
+++ main.cpp
@@ -50,16 +50,34 @@ struct SegmentColours {
 
 };
 
-struct Segment {
-    float r, theta, x, y;
-    float area;
-    float density;
+struct P {
+    double v, m;
 };
 
+struct Segment {
+    double r, theta, x, y;
+    double vr, vt;
+    double ar, at;
+    double area;
+    double m;
+    int n[8] = { -1, -1, -1, -1, -1, -1, -1, -1}; //nearest neighbours
+    std::vector<P> pr;
+    std::vector<P> pt;
+};
 
-int num_points = 300;
-float inner_radius = .39;
-float outer_radius = 40;
+#define G 6.6e-11
+int levels = 6;
+int num_points = pow(2, levels);
+double AU = 1.49e08;
+double inner_radius =  0.39 * AU;
+double outer_radius = 40.00 * AU;
+
+#define M_EARTH   1e22
+#define M_SUN     2e30
+#define M_JUPITER 1e27
+
+double stellar_mass = M_SUN;
+double escape_mass = 0;
 
 std::vector<SegmentVertices> segmentVertices;
 std::vector<SegmentColours> segmentColours;
@@ -102,7 +120,9 @@ void DestroyShaders(void);
 
 void PrintPoints();
 
-void MapDensityToColour();
+void MapSegmentToColor();
+
+void CalcSystemMass();
 
 int main(int argc, char *argv[]) {
     Initialize(argc, argv);
@@ -123,26 +143,50 @@ void Initialize(int argc, char *argv[]) {
     srand (time(NULL));
 
     // populate points
-    float theta_step = 2 * M_PI / num_points;
-    float theta_step_2 = theta_step / 2;
-    float radius_step = ( outer_radius - inner_radius ) / num_points;
-    float radius_step_2 = radius_step / 2;
+    double theta_step = 2 * M_PI / num_points;
+    double theta_step_2 = theta_step / 2;
+    double radius_step = ( outer_radius - inner_radius ) / num_points;
+    double radius_step_2 = radius_step / 2;
 
     for (size_t t = 0; t < num_points; t++) {
 
-        float theta = t * theta_step + theta_step_2;
+        double theta = t * theta_step + theta_step_2;
 
         for (size_t r = 0; r < num_points; r++) {
 
-            float radius = inner_radius + (r * radius_step) + radius_step_2;
+            double radius = inner_radius + (r * radius_step) + radius_step_2;
 
             Segment s;
+            s.pr.reserve(9);
+            s.pt.reserve(9);
             s.r = radius;
             s.theta = theta;
             s.x = radius * cos(theta);
             s.y = radius * sin(theta);
-            s.area = 1;
-            s.density = float(rand() % 64) / 255;
+            s.area = 2.0 * radius * radius_step * theta_step;
+            s.m = (double)(rand()%num_points) / 1 * M_EARTH * s.area; //double(rand() % 100000)/100000.0f * max_segment_mass / 1000.0f;
+            s.vr = 0;
+            s.vt = 0;
+            double v = 1e6*sqrt(G*s.m/s.r);
+            if (v > 0) {
+                s.vt = v / (2.0 * M_PI * s.r) ;
+            }
+
+            if ( r > 0 ) {
+                s.n[0] = (t * num_points) + r - 1;
+                s.n[1] = (((t+1)%num_points) * num_points) + r - 1;
+                s.n[7] = (((t+num_points-1)%num_points) * num_points) + r - 1;
+            }
+            if ( r < num_points - 1 ) {
+                s.n[3] = (((t+1)%num_points) * num_points) + r + 1;
+                s.n[4] = (t * num_points) + r + 1;
+                s.n[5] = (((t+num_points-1)%num_points) * num_points) + r + 1;
+
+            }
+            s.n[2] = (((t+1)%num_points) * num_points) + r;
+            s.n[6] = (((t+num_points-1)%num_points) * num_points) + r;
+
+
             segment.push_back(s);
             newSegment.push_back(s);
 
@@ -174,7 +218,7 @@ void Initialize(int argc, char *argv[]) {
             segmentVertices.push_back(sv);
 
             SegmentColours sc;
-            sc.c1.r = s.density;
+            sc.c1.r = s.m;
             sc.c1.g = sc.c1.r;
             sc.c1.b = sc.c1.r;
             sc.c1.a = 1.0f;
@@ -193,8 +237,8 @@ void Initialize(int argc, char *argv[]) {
             (unsigned) segmentVertices.size()
     );
 
-
     PrintPoints();
+    CalcSystemMass();
 
     InitWindow(argc, argv);
 
@@ -266,21 +310,322 @@ GLuint programID;
 GLuint MatrixID;
 glm::mat4 MVP;
 
-void CalcTotalMass() {
-    float mass = 0;
+void CalcSystemMass() {
+    double disk_mass = 0;
+    double pr = 0;
+    double pt = 0;
     for (size_t i = 0; i < segment.size(); i ++ ) {
-        mass += segment[i].density * segment[i].area;
+        disk_mass += segment[i].m;
+        pr += segment[i].m * segment[i].vr;
+        pt += segment[i].m * segment[i].vt;
     }
-    fprintf(stdout, "INFO: Total mass: %f\n", mass );
+    fprintf(stdout, "INFO: Stellar Mass: %.0f Disk Mass: %.0f : Total mass: %.0f, pr = %.0f, pt = %18.0f\n",
+            stellar_mass, disk_mass, stellar_mass+disk_mass, pr, pt );
+}
+
+#define AFAC 0.25f
+#define BFAC 0.5f
+
+void ApplyBruteForceGravity() {
+
+    // Calculate acceleration for all points
+    for (size_t r = 0; r < num_points; r++) {
+        for (size_t t = 0; t < num_points; t++) {
+
+                size_t i1 = (t * num_points) + r;
+
+            newSegment[i1].ar = 0;//-1e15 * stellar_mass * G / (segment[i1].r * segment[i1].r);
+            newSegment[i1].at = 0;
+
+/*
+            for (size_t st = 0; st < num_points; st++) {
+                for (size_t sr = 0; sr < num_points; sr++) {
+
+                    size_t i2 = (st * num_points) + sr;
+
+                    if (i2 != i1) {
+
+                        double x = segment[i2].x - segment[i1].x;
+                        double y = segment[i2].y - segment[i1].y;
+                        double d = sqrt((x * x) * (y * y));
+                        double alpha = atan(y/x);
+                        double f = d * d;
+                        double a = 0;
+                        if (f > 0) {
+                            a = G * segment[i2].m / f;
+                        }
+                        double dt = segment[i1].theta - alpha;
+                        newSegment[i1].at += a * sin(dt);
+                        newSegment[i1].ar += a * cos(dt);
+                        //fprintf( stdout, "INFO: %f, %f, %f, %f, %f, %f\n", x, y, d, alpha, f, a );
+
+                    }
+                }
+            }
+*/
+        }
+    }
+
+    // apply acceleration to all velocities
+    for ( int i = 0; i < newSegment.size(); i++) {
+        newSegment[i].vr += newSegment[i].ar;
+        newSegment[i].vt += newSegment[i].at;
+        newSegment[i].pr.clear();
+        newSegment[i].pt.clear();
+    }
+
+    // populate points
+    double radius_step = ( outer_radius - inner_radius ) / num_points;
+    double radius_step_2 = radius_step / 2;
+
+    // based on velocities, move the matter, conserve momentum
+    for (size_t r = 0; r < num_points; r++) {
+        for (size_t t = 0; t < num_points; t++) {
+
+            size_t i1 = (t * num_points) + r;
+
+            int * n = &newSegment[i1].n[0];
+
+            double theta_step = 2 * (double)M_PI * newSegment[i1].r / num_points;
+            double theta_step_2 = theta_step / 2;
+            double r1 = newSegment[i1].r - radius_step_2;
+            double r2 = r1 + newSegment[i1].vr;
+            double t1 = newSegment[i1].theta - theta_step_2;
+            double t2 = t1 + newSegment[i1].vt;
+
+            double a1 = 0;
+            double a2 = 0;
+            double a3 = 0;
+            int n1, n2, n3;
+            if ( r2 >= r1 ) {
+                if ( t2 >= t1 ) {
+                    // Q1
+                    a1 = min(radius_step, r2-r1)     *min(theta_step, t1+theta_step-t2);
+                    a2 = min(radius_step, r2-r1)     *min(theta_step, t2-t1);
+                    a3 = min(radius_step, r1+radius_step-r2) *min(theta_step, t2-t1);
+                    n1 = 4;
+                    n2 = 3;
+                    n3 = 2;
+                } else {
+                    // Q4
+                    a1 = min(radius_step, r2-r1)     *min(theta_step, t2+theta_step-t1);
+                    a2 = min(radius_step, r2-r1)     *min(theta_step, t1-t2);
+                    a3 = min(radius_step, r1+radius_step-r2) *min(theta_step, t1-t2);
+                    n1 = 4;
+                    n2 = 5;
+                    n3 = 6;
+                }
+            } else {
+                if ( t2 >= t1 ) {
+                    // Q2
+                    a1 = min(radius_step, r1-r2)     *min(theta_step, t1+theta_step-t2);
+                    a2 = min(radius_step, r1-r2)     *min(theta_step, t2-t1);
+                    a3 = min(radius_step, r2+radius_step-r1) *min(theta_step, t2-t1);
+                    n1 = 0;
+                    n2 = 1;
+                    n3 = 2;
+                } else {
+                    // Q3
+                    a1 = min(radius_step, r1-r2)     *min(theta_step, t2+theta_step-t1);
+                    a2 = min(radius_step, r1-r2)     *min(theta_step, t1-t2);
+                    a3 = min(radius_step, r2+radius_step-r1) *min(theta_step, t1-t2);
+                    n1 = 0;
+                    n2 = 7;
+                    n3 = 6;
+                }
+
+            }
+
+            // area 1
+            int i2 = n[n1];
+            double dm = newSegment[i1].m*a1/newSegment[i1].area;
+            double m1 = newSegment[i1].m;
+            if ( i2 < 0 ) {
+                if (n1 == 0 ) {
+                    stellar_mass += dm;
+                } else {
+                    escape_mass += dm;
+                }
+            } else {
+                newSegment[i2].pr.push_back({newSegment[i1].vr, dm});
+            }
+            m1 = max(0.0, m1-dm);
+
+            // area 2
+            i2 = n[n2];
+            dm = newSegment[i1].m*a2/newSegment[i1].area;
+            if ( i2 < 0 ) {
+                if (n2 == 1 || n2 == 7 ) {
+                    stellar_mass += dm;
+                } else {
+                    escape_mass += dm;
+                }
+            } else {
+
+                newSegment[i2].pr.push_back({newSegment[i1].vr, dm/2});
+                newSegment[i2].pt.push_back({newSegment[i1].vt, dm/2});
+
+            }
+            m1 = max(0.0, m1-dm);
+
+
+            // area 3
+            i2 = n[n3];
+            dm = newSegment[i1].m*a3/newSegment[i1].area;
+            newSegment[i2].pt.push_back({newSegment[i1].vt, dm});
+            m1 = max(0.0, m1-dm);
+
+            newSegment[i1].pr.push_back({newSegment[i1].vr, m1/2});
+            newSegment[i1].pt.push_back({newSegment[i1].vt, m1/2});
+
+        }
+    }
+    for ( int i = 0; i < newSegment.size(); i++) {
+        double p = 0.0, m = 0.0;
+        for( auto &it : newSegment[i].pt ) {
+            p += it.m * it.v;
+            m += it.m;
+        }
+        newSegment[i].vt = 0;
+        if ( m > 0 ) {
+            newSegment[i].vt = p / m;
+        }
+        newSegment[i].m = m;
+        p = 0.0, m = 0.0;
+        for( auto &it : newSegment[i].pr ) {
+            p += it.m * it.v;
+            m += it.m;
+        }
+        newSegment[i].vr = 0;
+        if ( m > 0 ) {
+            newSegment[i].vr = p / m;
+        }
+        newSegment[i].m += m;
+    }
+
+}
+
+void ApplyXNearestNeighbourGravity() {
+
+    std::vector<double> dd(num_points*num_points, 0.0f);
+    int span = 3;
+
+    for (size_t t = 0; t < num_points; t++) {
+
+        for (size_t r = 0; r < num_points; r++) {
+
+            size_t i1 = (t * num_points) + r;
+
+            double dr = -1/(segment[i1].r*segment[i1].r);
+            double dt = 0;
+
+            for ( int st = -span; st < span; st++) {
+                for ( int sr = -span; sr < span; sr++) {
+
+                    size_t tr = max( 0, min((int)num_points, (int)r+sr));
+                    size_t tt = ((t+num_points+st)%num_points);
+                    size_t i2 = (tt * num_points) + tr;
+
+                    if ( i2 != i1 ) {
+
+                        double x = segment[i2].x - segment[i1].x;
+                        double y = segment[i2].y - segment[i1].y;
+                        double d = sqrt((x*x)*(y*y));
+                        double f = d * d;
+                        double nr = 0;
+                        if (f > 0) {
+                            nr = segment[i2].m / f;
+                        }
+                        double nt = 0;
+                        if ( d > 0) {
+                            nt = asin(y / d);
+                        }
+                        dt += nr * sin(nt);
+                        dr += nr * cos(nt);
+                    }
+                }
+            }
+
+            int n[8] = { -1, -1, -1, -1, -1, -1, -1, -1};
+            if ( r > 0 ) {
+                n[0] = (t * num_points) + r - 1;
+                n[1] = (((t+1)%num_points) * num_points) + r - 1;
+                n[7] = (((t+num_points-1)%num_points) * num_points) + r - 1;
+            }
+            if ( r < num_points - 1 ) {
+                n[3] = (((t+1)%num_points) * num_points) + r + 1;
+                n[4] = (t * num_points) + r + 1;
+                n[5] = (((t+num_points-1)%num_points) * num_points) + r + 1;
+
+            }
+            n[2] = (((t+1)%num_points) * num_points) + r;
+            n[6] = (((t+num_points-1)%num_points) * num_points) + r;
+
+
+            double d = sqrt(dr*dr+dt*dt);
+            d = min( segment[i1].m, d );
+            dd[i1] -= d;
+
+            double pdr;
+            double pdt;
+            double pc = (abs(dr)+abs(dt));
+            if ( pc > 0 ) {
+                pdr = d * abs(dr) / (abs(dr) + abs(dt));
+                pdt = d * abs(dt) / (abs(dr) + abs(dt));
+            } else {
+                pdr = 0;
+                pdt = 0;
+            }
+
+            if ( dr > 0 ) {
+                if( n[3] >= 0 ) {
+                    dd[n[3]] += AFAC * pdr;
+                    dd[n[4]] += BFAC * pdr;
+                    dd[n[5]] += AFAC * pdr;
+                }
+            } else {
+                if( n[1] >= 0 ) {
+                    dd[n[1]] += AFAC * pdr;
+                    dd[n[0]] += BFAC * pdr;
+                    dd[n[7]] += AFAC * pdr;
+                }
+            }
+
+            if ( dt > 0 ) {
+                dd[n[2]] += BFAC * pdt;
+                if( n[1] >= 0 ) {
+
+                    dd[n[1]] += AFAC * pdt;
+                }
+                if( n[3] >= 0 ) {
+                    dd[n[3]] += AFAC * pdt;
+                }
+            } else {
+                if( n[7] >= 0 ) {
+                    dd[n[7]] += AFAC * pdt;
+                }
+                    if( n[5] >= 0 ) {
+                        dd[n[5]] += AFAC * pdt;
+                    }
+                dd[n[6]] += BFAC * pdt;
+            }
+
+        }
+    }
+
+    for ( int i = 0; i < newSegment.size(); i++) {
+        newSegment[i].m = max( 0.0, newSegment[i].m + 1e16*dd[i]);
+    }
 }
 
 
-float r2 = 1/sqrt(2);
+
+double r2 = 1/sqrt(2);
 
 
 void ApplyNearestNeighbourGravity() {
 
-    std::vector<float> dd(num_points*num_points, 0.0f);
+    std::vector<double> dd(num_points*num_points, 0.0);
 
 
     for (size_t t = 0; t < num_points; t++) {
@@ -299,21 +644,21 @@ void ApplyNearestNeighbourGravity() {
             n[7] = (((t+num_points-1)%num_points) * num_points) + r - 1;
 
             // radial component
-            float dr =   ( r2 * segment[n[3]].density + segment[n[4]].density + r2 * segment[n[5]].density )
-                       - ( r2 * segment[n[1]].density + segment[n[0]].density + r2 * segment[n[7]].density )
+            double dr =   ( r2 * segment[n[3]].m + segment[n[4]].m + r2 * segment[n[5]].m )
+                       - ( r2 * segment[n[1]].m + segment[n[0]].m + r2 * segment[n[7]].m )
                        - 10/(segment[i].r*segment[i].r);
 
             // angular component
-            float dt =   ( r2 * segment[n[1]].density + segment[n[2]].density + r2 * segment[n[3]].density )
-                       - ( r2 * segment[n[5]].density + segment[n[6]].density + r2 * segment[n[7]].density );
+            double dt =   ( r2 * segment[n[1]].m + segment[n[2]].m + r2 * segment[n[3]].m )
+                       - ( r2 * segment[n[5]].m + segment[n[6]].m + r2 * segment[n[7]].m );
 
-            float d = sqrt(dr*dr+dt*dt);
-            d = min( segment[i].density, d );
+            double d = sqrt(dr*dr+dt*dt);
+            d = min( segment[i].m, d );
             dd[i] -= d;
 
-            float pdr;
-            float pdt;
-            float pc = (abs(dr)+abs(dt));
+            double pdr;
+            double pdt;
+            double pc = (abs(dr)+abs(dt));
             if ( pc > 0 ) {
                 pdr = d * abs(dr) / (abs(dr) + abs(dt));
                 pdt = d * abs(dt) / (abs(dr) + abs(dt));
@@ -345,7 +690,7 @@ void ApplyNearestNeighbourGravity() {
     }
 
     for ( int i = 0; i < newSegment.size(); i++) {
-        newSegment[i].density = max( 0.0f, newSegment[i].density + dd[i]);
+        newSegment[i].m = max( 0.0, newSegment[i].m + dd[i]);
     }
 }
 
@@ -367,13 +712,13 @@ void RenderFunction(void) {
     glFlush();
     glutSwapBuffers();
 
-    ApplyNearestNeighbourGravity();
+    ApplyBruteForceGravity();
 
     segment = newSegment;
 
-    CalcTotalMass();
+    CalcSystemMass();
 
-    MapDensityToColour();
+    MapSegmentToColor();
 
     glBindBuffer(GL_ARRAY_BUFFER, VboAreaId);
     glBufferData(GL_ARRAY_BUFFER, segmentVertices.size() * sizeof(SegmentVertices), &segmentVertices[0], GL_STATIC_DRAW);
@@ -387,21 +732,25 @@ void RenderFunction(void) {
 
 }
 
-void MapDensityToColour() {
+void MapSegmentToColor() {
 
-/*
-    float maxDensity = 1.0;
+    double d_max = 0;
+    double d_min = stellar_mass;
+
     for (int i = 0; i < segment.size(); i++) {
-        if (segment[i].density > maxDensity) {
-            maxDensity = segment[i].density;
+        double d = segment[i].m/segment[i].area;
+        if( d > d_max ) {
+            d_max = d;
+        }
+        if ( d < d_min ) {
+            d_min = d;
         }
     }
-*/
-
+    double d_range = d_max - d_min;
 
     for (int i = 0; i < segmentColours.size(); i++) {
         SegmentColours* scp = &segmentColours[i];
-        scp->c1.r = min( segment[i].density, 1.0f );
+        scp->c1.r = (float)((segment[i].m/segment[i].area-d_min)/d_range);
         scp->c1.g = scp->c1.r;
         scp->c1.b = scp->c1.r;
         scp->c2 = scp->c1;
@@ -505,7 +854,7 @@ void CreateShaders(void) {
     // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
     //glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
     // Or, for an ortho camera :
-    float ortho = outer_radius;
+    float ortho = (float)outer_radius;
     float factor = 1.1f;
 
     glm::mat4 Projection = glm::ortho(-1.0f * factor * ortho, factor * ortho, -1.0f * factor * ortho, factor * ortho, -1.0f, 1.0f); // In world coordinates
