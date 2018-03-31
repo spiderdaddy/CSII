3159b41aadad99826eae3ef24449a713dc86b661
Lucas Fowler <lucas.fowler@macd.com>
2018-03-31 03:59:33 +0200

    - Added ability to only look at n nearest neighbours. No edge calcs so only for quick debug.

--- GravityProvider/PolarBruteForceSelfGravityProvider.cpp
+++ GravityProvider/PolarBruteForceSelfGravityProvider.cpp
@@ -10,38 +10,45 @@
 
 void PolarBruteForceSelfGravityProvider::calculate() {
 
-    std::vector<Disk::Segment>& newSegment = *GravityProvider::pNewSegment;
-    std::vector<Disk::Segment>& segment = *GravityProvider::pSegment;
+    int nearestNeighbours = max(GravityProvider::num_radial_cells, GravityProvider::num_azimuthal_cells) + 1;
+    std::vector<Disk::Segment> &newSegment = *GravityProvider::pNewSegment;
+    std::vector<Disk::Segment> &segment = *GravityProvider::pSegment;
 
     // Calculate acceleration for all points
-    for (std::size_t r = 0; r < GravityProvider::num_radial_cells; r++) {
-        for (std::size_t t = 0; t < GravityProvider::num_azimuthal_cells; t++) {
+    for (int r = 0; r < GravityProvider::num_radial_cells; r++) {
+        for (int t = 0; t < GravityProvider::num_azimuthal_cells; t++) {
 
-            std::size_t i1 = (r * GravityProvider::num_azimuthal_cells) + t;
+            int i1 = (r * GravityProvider::num_azimuthal_cells) + t;
 
             double ar = 0;
             double at = 0;
 
-            for (std::size_t sr = 0; sr < GravityProvider::num_radial_cells; sr++) {
-                for (std::size_t st = 0; st < GravityProvider::num_azimuthal_cells; st++) {
+            int min_r = std::max(0, r - nearestNeighbours);
+            int max_r = min(r + nearestNeighbours, GravityProvider::num_radial_cells);
+            int  min_t = std::max(0, t - nearestNeighbours);
+            int  max_t = min(t + nearestNeighbours, GravityProvider::num_azimuthal_cells);
+            for (int sr = min_r; sr < max_r; sr++) {
+                for (int st = min_t; st < max_t; st++) {
 
-                    std::size_t i2 = (sr * GravityProvider::num_azimuthal_cells) + st;
+                    int i2 = (sr * GravityProvider::num_azimuthal_cells) + st;
 
                     if (i2 != i1) {
                         double diff_theta = segment[i1].theta - segment[i2].theta;
                         double cos_diff_theta = cos(diff_theta);
                         double diff_r = segment[i1].r - segment[i2].r;
                         double exp_diff_r = exp(diff_r);
-                        double distance = exp_diff_r * exp_diff_r
-                                + 1
-                                - ( 2 * exp_diff_r * cos_diff_theta );
-                        distance = distance * sqrt(distance);
-                        double a = 0;
-                        if (distance > 0) {
-                            a = G * segment[i2].density / distance;
+                        double distance = (exp_diff_r * exp_diff_r)
+                                          + 1
+                                          - (2 * exp_diff_r * cos_diff_theta);
+                        distance = distance * sqrt(abs(distance));
+                        fflush(stdout);
+
+                        double a = 0.0;
+                        if (distance != 0.0) {
+                            a = -G * segment[i2].density * segment[i2].area / distance;
                         }
-                        ar += a * ( exp_diff_r - cos_diff_theta );
-                        ar += a * sin(diff_theta) ;
+                        ar += a * (exp_diff_r - cos_diff_theta);
+                        at += a * sin(diff_theta);
                     }
                 }
             }
@@ -52,7 +59,7 @@ void PolarBruteForceSelfGravityProvider::calculate() {
         }
         fprintf(
                 stdout,
-                "INFO: r=%4lu\r",
+                "INFO: r=%4d\r",
                 r
         );
         fflush(stdout);
